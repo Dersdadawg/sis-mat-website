df4eb7cb16adbb4396523060dc067d250fc4afd9
Dersdadawg <andicore100@gmail.com>
2026-04-11 22:38:08 +0900

    got rid of gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,6 @@ export default function HomePage() {
             priority
             unoptimized
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-[hsl(var(--background))]" />
         </div>
 
         <div
